0858631604ab0035e7b3e575eb73cd7da4283352
Abdelkrim a <karimusthb@gmail.com>
2017-03-21 14:58:53 +0100

    fix

--- app/app.js
+++ app/app.js
@@ -15,12 +15,14 @@
 	app.directive('orsActive', function() {
 		return {
 			restrict: 'A',
-			controller: ['$scope', 'attrs', '$element', '$location', function($scope, $attrs, $element, $location) {
+			controller: ['$scope', '$attrs', '$element', '$location', function($scope, $attrs, $element, $location) {
 				console.log('attrs', $attrs);
 
 				$scope.$on('$routeChangeStart', function(next, current) {
+
 					var url = $attrs.href;
 					var path = '#' + $location.path();
+					console.log('test')
 					if (url === path) {
 						$element.addClass('active');
 					} else {
